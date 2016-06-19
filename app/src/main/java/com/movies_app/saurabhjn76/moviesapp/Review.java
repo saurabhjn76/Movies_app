75c986613fd54e80061c6aee867afa7f48202c4f
saurabhjn76 <saurabhjn76@gmail.com>
2016-06-20 03:02:04 +0530

    Added Revies class

--- app/src/main/java/com/movies_app/saurabhjn76/moviesapp/Review.java
+++ app/src/main/java/com/movies_app/saurabhjn76/moviesapp/Review.java
@@ -4,4 +4,26 @@ package com.movies_app.saurabhjn76.moviesapp;
  * Created by saurabh on 20/6/16.
  */
 public class Review {
+    private String author;
+    private String content;
+    private String url;
+
+
+    Review(String author,String content,String url){
+            this.author=author;
+            this.content=content;
+            this.url=url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
